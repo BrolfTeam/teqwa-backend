045bdb71e7484c5ed74d1215305746c9a23657c9
BrolfTeam <abcanwar88@gmail.com>
2025-12-26 06:36:56 +0300

    fixed

--- fix_migrations.sh
+++ fix_migrations.sh
@@ -2,7 +2,7 @@
 echo "Fixing database migrations..."
 
 # Make migrations for specific apps that showed changes
-docker compose exec backend python manage.py makemigrations authentication education students
+docker compose exec backend python manage.py makemigrations authentication donations education futsal_booking itikaf memberships students
 
 # Make migrations for everything else just in case
 docker compose exec backend python manage.py makemigrations
